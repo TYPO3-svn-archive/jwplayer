4b0852d2c9a4b54b633989d22eb6fbfd9fe80e30
hacksch <hacksch@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-27 12:36:05 +0000

    add Jw Player as CType
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jwplayer/trunk@47045 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -6,8 +6,20 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 	'JW Player'
 );
 
+t3lib_div::loadTCA('tt_content');
+
+$GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'][] = array(
+    0 => 'LLL:EXT:jwplayer/locallang.xml:jwplayer.title',
+    1 => 'jwplayer',
+    2 => 'i/tt_content_image.gif',
+);
+
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['jwplayer_pi1'] = 'layout,recursive,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['jwplayer_pi1'] = 'pi_flexform';
 t3lib_extMgm::addPiFlexFormValue( 'jwplayer_pi1', 'FILE:EXT:jwplayer/Configuration/FlexForms/Player.xml');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'JW Player JS Files');
+
+if (TYPO3_MODE=='BE')    {
+    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_jwplayer_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'classes/class.tx_jwplayer_wizicon.php';
+}
 ?>
