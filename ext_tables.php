ee9a3ad3e9b64727837cef3caa2aa52388d291f4
hacksch <hacksch@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-27 15:08:33 +0000

    add to wizard by pageTS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jwplayer/trunk@47054 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -6,20 +6,34 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 	'JW Player'
 );
 
+t3lib_extMgm::addPageTSConfig('
+	mod.wizards.newContentElement.wizardItems.header {
+		header = LLL:EXT:jwplayer/locallang.xml:jwplayer.title
+		elements.jwplayer {
+			icon = ../typo3conf/ext/jwplayer/tt_content_jwplayer.gif
+			title = LLL:EXT:jwplayer/locallang.xml:jwplayer.title
+			description = LLL:EXT:jwplayer/locallang.xml:jwplayer.description
+			tt_content_defValues {
+				CType = jwplayer_pi1
+			}
+		}
+	}
+	mod.wizards.newContentElement.wizardItems.header.show = JW Player
+
+');
+
+
+
 t3lib_div::loadTCA('tt_content');
-
-
-t3lib_extMgm::addPlugin(array('LLL:EXT:jwplayer/locallang.xml:jwplayer.title', 'jwplayer_pi1', 'EXT:jwplayer/ext_icon.gif'), 'CType');
-$TCA['tt_content']['types']['jwplayer_pi1']['showitem'] = 'CType;;4;pi_flexform';
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['jwplayer_pi1'] = 'layout,recursive,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['jwplayer_pi1'] = 'pi_flexform';
 t3lib_extMgm::addPiFlexFormValue( 'jwplayer_pi1', 'FILE:EXT:jwplayer/Configuration/FlexForms/Player.xml');
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'JW Player JS Files');
 
-$TCA['tt_content']['columns']['pi_flexform']['config']['ds_pointerField'] = 'list_type,CType';
-$TCA['tt_content']['columns']['pi_flexform']['config']['ds'][',jwplayer_pi1'] = 'FILE:EXT:jwplayer/Configuration/FlexForms/Player.xml';
 
+$TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem']='CType;;4;button;1-1-1, header;;3;;2-2-2,pi_flexform;;;;1-1-1';
+$TCA['tt_content']['columns']['pi_flexform']['config']['ds'][','.$_EXTKEY.'_pi1'] = 'FILE:EXT:jwplayer/Configuration/FlexForms/Player.xml';
 
 if (TYPO3_MODE=='BE')    {
     $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_jwplayer_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'Classes/class.tx_jwplayer_wizicon.php';
