9b7314e88c9b1aef3195b1d97cccb0ae3991f9e9
hacksch <hacksch@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-02 15:06:18 +0000

    add function to check available client formats change flexform
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jwplayer/trunk@47221 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/Js/tx_jw_player.js
+++ Resources/Public/Js/tx_jw_player.js
@@ -15,6 +15,21 @@ var tx_jwplayer = {
 			var clickname = window.webtrekk.contentId + '.movie.'+filename;
 			wt_sendinfo(clickname,'click');
 		}
+	},
+	checkAvailableType:function() {
+		var v = document.createElement('video');
+		var vtype = new Array();
+   
+   		if(!!(v.canPlayType && v.canPlayType('video/webm; codecs="vp8, vorbis"').replace(/no/, '')))
+	    	vtype.push('webm');
+		if(!!(v.canPlayType && v.canPlayType('video/ogg; codecs="theora, vorbis"').replace(/no/, '')))
+			vtype.push('ogv');
+	   	if(!!(v.canPlayType && v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"').replace(/no/, '')))
+	    	vtype.push('mp4');	   	
+	    	
+	    if(vtype.length == 0) return false;
+	 	   
+	   	return vtype;
 	}
 }
 
