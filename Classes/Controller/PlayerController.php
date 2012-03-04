4b50a60995f26bbe8559db9483da4a39d1124e67
hacksch <hacksch@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-04 14:07:17 +0000

    remove single simikolon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jwplayer/trunk@58927 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/PlayerController.php
+++ Classes/Controller/PlayerController.php
@@ -376,7 +376,7 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	
 		$extPath = t3lib_extMgm::siteRelPath ( 'jwplayer' );
 		$file = $extPath . 'Resources/Public/Player/jwplayer.js';
-		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterFile( $file);
+		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterFile( $file );
 		$file = $extPath . 'Resources/Public/Js/tx_jw_player.js';
 		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterFile( $file );	
 	}
