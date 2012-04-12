cf994132c2778e6d7dd243c640117049747269cb
hacksch <hacksch@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-12 14:10:25 +0000

    fix preview image path
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jwplayer/trunk@60603 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/PlayerController.php
+++ Classes/Controller/PlayerController.php
@@ -278,7 +278,7 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 						$GLOBALS['TSFE']->getPageRenderer()->addMetaTag( '<meta property="og:title" content="'.htmlspecialchars($title).'"/>' );
 	
 						# TODO: Render image to 50x50 PX
-						$imgPath = t3lib_div::getIndpEnv('TYPO3_SITE_URL') . $previewImagePath ;
+						$imgPath = $this->removeLastChar(t3lib_div::getIndpEnv('TYPO3_SITE_URL')) . $previewImagePath;
 						$GLOBALS['TSFE']->getPageRenderer()->addMetaTag( '<meta property="og:image" content="'.$imgPath.'">' );
 					}
 				}
@@ -410,7 +410,7 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	private function getUploadPath( $filename  = NULL) {
 		$path = '';
 		if( $filename ){
-			$path = self::UPLOAD_PATH . $filename;
+			$path = '/' . self::UPLOAD_PATH . $filename;
 		}
 		return $path;
 	}
