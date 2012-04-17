f72ceb20a70f20577b6b0fbb1dc3386795517f46
kj187 <kj187@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-17 14:12:13 +0000

    Fix javascript includes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jwplayer/trunk@60823 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/PlayerController.php
+++ Classes/Controller/PlayerController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Player 
+ * Player
  * @package jwplayer
  */
 class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_ActionController {
@@ -8,28 +8,28 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	 * define separators for params and their values
 	 */
 	const UPLOAD_PATH = 'uploads/tx_jwplayer/';
-	
+
 	var $allowedSkinExtension = array(
 		'zip',
 		'swf',
 		'xml'
 	);
-	
+
 	/**
 	 * @var array
 	 */
 	private $conf;
-	
+
 	/**
 	 * @var Tx_Jwplayer_FlashConfigGenerator
 	 */
 	private $flashConfigGenerator;
-	
+
 	/**
 	 * View to render inline JS
 	 */
 	private $jsView;
-	
+
 	/**
 	 * Initializes the current action
 	 *
@@ -37,7 +37,7 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	 */
 	protected function initializeAction() {
 		$this->flashConfigGenerator = t3lib_div::makeInstance ( 'Tx_Jwplayer_FlashConfigGenerator' );
-		
+
 		$this->conf = t3lib_div::makeInstance ( 'Tx_Jwplayer_Configuration_ExtensionManager' );
 	}
 
@@ -47,16 +47,16 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	 */
 	public function indexAction() {
 
-		
+
 		if( $this->getSetting('disableJsAutoInclude') != 1 ) {
 			$this->addJavaScript();
 			$this->view->assign ( 'usePageRenderer', true);
 		} else {
 			$this->view->assign ( 'usePageRenderer', false);
 		}
-		
+
 		$playerId = uniqid('player');
-		
+
 		$this->view->assign ( 'player_id', $playerId);
 		$this->view->assign ( 'flashplayer', $this->conf->getPlayerPath());
 		$this->view->assign ( 'backcolor', $this->conf->getBackcolor() );
@@ -76,7 +76,7 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 		$this->view->assign ( 'playlist_position', $this->getPlaylistPosition() );
 		$this->view->assign ( 'playlist_size', $this->getSetting( 'playlistsize' ) );
 		$this->view->assign ( 'skin', $this->getSkin() );
-		
+
 		$this->setPlaylist();
 
 		if((boolean) $this->settings['add_metatags'] === TRUE) {
@@ -88,29 +88,29 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 			$GLOBALS['TSFE']->getPageRenderer()->addMetaTag( '<meta property="og:video:type" content="application/x-shockwave-flash"/>' );
 		}
 	}
-	
-	
+
+
 	/**
 	 *	Return setting, selected by name
 	 *	@parameter	string	$name
 	 *	@return 	string
 	 */
 	protected function getSetting( $name ) {
-		
+
 		$value = ( $this->settings['overrideGlobal'][ $name ] ) ? $this->settings['overrideGlobal'][ $name ] : $this->settings[ $name ];
-		
+
 		return $value;
 	}
-	
+
 	/**
 	 *	Return path to skin file when is set
 	 *
 	 *	@return string
 	 */
 	protected function getSkin() {
-	
+
 		$skinFile = '';
-	
+
 		if( file_exists( PATH_site . $this->conf->getSkin() ) && $this->checkSkinFileExtension( PATH_site . $this->conf->getSkin() ) ) {
         	$skinFile = $this->conf->getSkin();
 		} elseif( file_exists( PATH_site . $this->getSetting( 'skin' ) ) && $this->checkSkinFileExtension( PATH_site . $this->getSetting( 'skin' ) ) ) {
@@ -118,84 +118,84 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 		} else {
 			$skinFile = $this->getUploadPath( $this->getSetting( 'skin' ) );
 		}
-		
+
 		return $skinFile;
 	}
-	
+
 	/**
 	 *	Check if file extension is allow
 	 *	@param	string	$file
 	 *	@return	bool
 	 */
 	protected function checkSkinFileExtension( $file) {
-		
+
 		return in_array( pathinfo( PATH_site . $file, PATHINFO_EXTENSION ) ,$this->allowedSkinExtension );
 	}
-	
+
 	/**
 	 *	Return setting for playlist position
 	 *	When less than 2 items are available don't show playlist
 	 *	@return	string
 	 */
 	protected function getPlaylistPosition() {
-	
+
 		$position = $this->settings['playlistposition'];
-		
+
 		if( !$this->hasPlaylist() ) {
 			$position = 'none';
 		}
-		
+
 		return $position;
 	}
-	
+
 	/**
 	 *	Check if playlist should be shown
-	 *	@return	bool	
+	 *	@return	bool
 	 */
 	protected function hasPlaylist() {
-	
+
 		$flag = false;
-		
+
 		if( count( $this->getSetting( 'moviesection' ) ) > 1 ) {
 			$flag = true;
 		}
-		
+
 		return $flag;
 	}
-	
+
 	/**
 	 *	Calculate width of player
-	 *	@return	integer	
+	 *	@return	integer
 	 */
 	protected function getPlayerWidth() {
-	
+
 		$width = $this->getSetting( 'width' );
-		
+
 		if ( $this->hasPlaylist() && ( $this->getSetting( 'playlistposition' ) == 'left' || $this->getSetting( 'playlistposition' ) == 'right' ) ) {
 			$width = $width + $this->getSetting( 'playlistsize' );
 		}
-		
+
 		return $width;
 	}
-	
+
 	/**
 	 *	Calculate height of player
-	 *	@return	integer	
+	 *	@return	integer
 	 */
 	protected function getPlayerHeight() {
-	
+
 		$height = $this->getSetting( 'height' );
-		
+
 		if ( $this->hasPlaylist() && ( $this->getSetting( 'playlistposition' ) == 'top' || $this->getSetting( 'playlistposition' ) == 'bottom' ) ) {
 			$height = $height + $this->getSetting( 'playlistsize' );
 		}
-		
+
 		return $height;
 	}
-	
+
 	/**
 	 * show video (we need this action to be able to share the video in facebook)
-	 * 
+	 *
 	 * @param string $flash_player_config
 	 */
 	public function showVideoAction($flash_player_config) {
@@ -210,7 +210,7 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 		$flashPlayerConfig['fontcolor'] = $this->conf->getFontcolor();
 		$flashPlayerConfig['lightcolor'] = $this->conf->getLightcolor();
 		$flashPlayerConfig['screencolor'] = $this->conf->getScreenscolor();
-		
+
 		$skinFile = $this->getSkin();
 		if($skinFile != '' ) {
 			if(substr(0,1,$skinFile) != '/') {
@@ -218,11 +218,11 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 			}else {
 				$baseUrl = $this->removeLastChar($typo3SiteUrl);
 			}
-			
+
 			$skinUrl 					= $baseUrl.$skinFile;
 			$flashPlayerConfig['skin'] 	= $skinUrl;
 		}
-		
+
 		foreach($flashPlayerConfig as $key => $val) {
 			$flashPlayerUrl .= $key.'='.$val.'&';
 		}
@@ -234,18 +234,18 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	 *	Check count of files and create a playlist
 	 */
 	private function setPlayList() {
-	
+
 			/*
 			 * 	check count of movies. generate playliste when more than
 			 * 	one movie was set
 			 */
-		if ( count( $this->settings['moviesection'] ) > 1 ) {  
-		
+		if ( count( $this->settings['moviesection'] ) > 1 ) {
+
 			# TODO create Playlist
 			# now only the first movie will use
 
 			foreach( $this->settings['moviesection'] as $item ) {
-			
+
 				$movie = $this->getMovieList( $item );
 				$playlist[] = array(
 					'title' => $item['movieitem']['title'],
@@ -255,52 +255,52 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 					'image' => $this->getUploadPath( $item['movieitem']['image'] )
 				);
 			}
-			
+
 			$this->view->assign( 'playlistItems', $playlist );
 
 		} else {
 			if(is_array($this->settings['moviesection'])){
 				$itemArray = array_shift( $this->settings['moviesection'] );
-			
+
 				if( $movieList = $this->getMovieList( $itemArray ) ) {
-				
+
 					$previewImagePath = $this->getUploadPath( $itemArray['movieitem']['image'] );
-	
+
 					$this->view->assign ( 'file_flash', ( !empty( $movieList['flash'] ) ) ? $movieList['flash'] : $movieList['url'] );
 					$this->view->assign ( 'files_html5', $movieList['html5'] );
-					
+
 					$this->view->assign ( 'image', $previewImagePath );
-	
-						// add movie specific meta tags for facebook	
+
+						// add movie specific meta tags for facebook
 					if((boolean) $this->settings['add_metatags'] === TRUE) {
-	
+
 						$title = empty($this->settings['metatag_title']) ? $itemArray['movieitem']['file'] : $this->settings['metatag_title'];
 						$GLOBALS['TSFE']->getPageRenderer()->addMetaTag( '<meta property="og:title" content="'.htmlspecialchars($title).'"/>' );
-	
+
 						# TODO: Render image to 50x50 PX
 						$imgPath = $this->removeLastChar(t3lib_div::getIndpEnv('TYPO3_SITE_URL')) . $previewImagePath;
 						$GLOBALS['TSFE']->getPageRenderer()->addMetaTag( '<meta property="og:image" content="'.$imgPath.'">' );
 					}
 				}
 			}
-			
-		}	
-		
+
+		}
+
 	}
-	
+
 	/**
 	 *	Generate a list with given movie formats
-	 *	@param	
-	 */	
+	 *	@param
+	 */
 	protected function getMovieList( $itemArray ) {
-	
+
 		$movieArray = array(
 			'flash' => '',
 			'html5' => array(),
 			'url' => ''
 		);
-		
-			
+
+
 			// flashhtml5
 		if( $itemArray['movieitem']['file_flashhtml5'] ) {
 			$flashhtml5 = $this->solveMoviePath( $itemArray['movieitem']['file_flashhtml5'] );
@@ -326,14 +326,14 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 
 			// url
 		$movieArray['url'] = $this->solveMoviePath( $itemArray['movieitem']['url'], 'url' );
-	
+
 		if( empty( $movieArray['flash'] ) && empty( $movieArray['url'] ) ) {
-			return false;	
+			return false;
 		}
-		
+
 		return $movieArray;
 	}
-	
+
 	/**
 	 *	Create file path, by URL checks the syntax
 	 *	@param	string	$filename
@@ -341,9 +341,9 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	 *	@return	string
 	 */
 	protected function solveMoviePath( $filename, $type='file' ) {
-	
+
 		$filePath = '';
-	
+
 		if( !empty( $filename ) ) {
 			switch( $type ) {
 				case 'file':
@@ -354,24 +354,24 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 					break;
 			}
 		}
-			
+
 		return $filePath;
 	}
-	
+
 	/**
-	 *	Check Url 
+	 *	Check Url
 	 *
 	 *	@param	string	$url
 	 *	@return	bool
 	 */
 	private function checkUrl( $url ) {
-	
+
 		$flag = false;
-	
+
 		if( preg_match("/[h][t]{2}[p][\:][\/]{2}[w.0-9]{0,4}[a-zA-Z0-9.-]{2,40}[.][a-zA-Z]{2,7}/", $url ) ) {
 			$flag = true;
 		}
-		
+
 		return $flag;
 	}
 
@@ -379,13 +379,11 @@ class Tx_Jwplayer_Controller_PlayerController extends Tx_Extbase_MVC_Controller_
 	 * add javascript to page
 	 */
 	protected function addJavaScript() {
-	
+		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterLibrary('jwplayer', $this->conf->getJsPath());
+
 		$extPath = t3lib_extMgm::siteRelPath ( 'jwplayer' );
-		$file = $extPath . 'Resources/Public/Player/jwplayer.js';
-		#$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterFile( $file );
-        $GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterLibrary('jwplayer', $this->conf->getJsPath());
-        #$file = $extPath . 'Resources/Public/Js/tx_jw_player.js';
-        $GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterFile( $file ); 
+		$file = $extPath . 'Resources/Public/Js/tx_jw_player.js';
+		$GLOBALS ['TSFE']->getPageRenderer ()->addJsFooterFile( $file );
 	}
 	/**
 	 * create URL to action 'showVideo'
